2eebac1505745c45b407d8cb327a5267e7816b55
ZackTechLtd <za1012001@yahoo.co.uk>
2022-06-29 17:25:00 +0100

    Added EF Tools

--- LAHub/Domain/Entities/Service.cs
+++ LAHub/Domain/Entities/Service.cs
@@ -9,13 +9,15 @@ public class Service : BaseAuditableEntity<Guid>
     public Service(
         string name,
         string? description,
-        Guid organisationId
+        Guid organisationId,
+        Organisation? organisation
     )
     {
         Id = Guid.NewGuid();
         Name = name;
         Description = description ?? string.Empty;
         OrganisationId = organisationId;
+        Organisation = organisation;
     }
 
     public string Name { get; set; }
@@ -23,6 +25,6 @@ public class Service : BaseAuditableEntity<Guid>
     public string? Description { get; set; }
 
     public Guid OrganisationId { get; set; }
-
+    public virtual Organisation? Organisation { get; set; } = default!;
     public ICollection<ServiceLocation> ServiceLocations { get; set; } = default!;
 }
