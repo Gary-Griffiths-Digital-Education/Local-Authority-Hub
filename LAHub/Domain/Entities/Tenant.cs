b17c05f2b5b272aacbb18abc213c197fb8c01465
ZackTechLtd <za1012001@yahoo.co.uk>
2022-07-07 13:46:43 +0100

    Added Some Changed entities

---
+++ LAHub/Domain/Entities/Tenant.cs
@@ -0,0 +1,21 @@
+﻿namespace LAHub.Domain.Entities
+{
+    public class Tenant : BaseAuditableEntity<Guid>
+    {
+        public Tenant() { }
+
+        public Tenant(
+                string name,
+                string? description
+            )
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description ?? string.Empty;
+        }
+
+        public string Name { get; set; } = String.Empty;
+
+        public string? Description { get; set; } = String.Empty;
+    }
+}
