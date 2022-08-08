56fcbc5bb79984eb5aa0d9527b7edc50c83cbdcb
ZackTechLtd <za1012001@yahoo.co.uk>
2022-08-08 09:08:47 +0100

    Added Get Open Referral Service by Id

---
+++ LAHub/Application/Commands/GetOpenReferralService/GetOpenReferralServiceByIdCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Commands.GetOpenReferralService;
+
+public class GetOpenReferralServiceByIdCommandValidator : AbstractValidator<GetOpenReferralServiceByIdCommand>
+{
+    public GetOpenReferralServiceByIdCommandValidator()
+    {
+        RuleFor(v => v.Id)
+            .NotNull()
+            .NotEmpty();
+    }
+}
